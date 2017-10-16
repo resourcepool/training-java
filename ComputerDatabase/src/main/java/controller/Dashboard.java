2d8ada3b3c95d47da1076a52f18080f94dd73525
sebeq33 <sbequignon@ebiz.fr>
2017-10-16 15:57:53 +0200

    first taglib, first servlet

--- ComputerDatabase/src/main/java/controller/Dashboard.java
+++ ComputerDatabase/src/main/java/controller/Dashboard.java
@@ -1,15 +1,21 @@
 package controller;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
- * Servlet implementation class Dashboard
+ * Servlet implementation class Dashboard.
  */
+@WebServlet
 public class Dashboard extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -17,22 +23,75 @@ public class Dashboard extends HttpServlet {
      * Default constructor.
      */
     public Dashboard() {
-        // TODO Auto-generated constructor stub
     }
 
     /**
+     * {@inheritDoc}
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     * @param request request
+     * @param response response
+     * @throws ServletException request
+     * @throws IOException exception
      */
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+
+        CharSequence files = getFiles();
+        request.setAttribute("real_source", files);
+        request.getRequestDispatcher("/WEB-INF/dashboard.jsp").forward(request, response);
+
     }
 
     /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     * @return iterate at "." to list all files
      */
+    private CharSequence getFiles() {
+        Collection<File> all = new ArrayList<File>();
+        StringBuilder sb = new StringBuilder();
+
+        File currentfile = new File(".");
+        addTree(currentfile, all);
+
+        sb.append(currentfile.getAbsolutePath() + System.lineSeparator());
+        sb.append("<br/>");
+        for (File file : all) {
+            sb.append(file.getPath());
+            sb.append("<br/>");
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * @param file directory
+     * @param all colection of path found
+     * @throws IOException fail to read directory
+     */
+    private static void addTree(File file, Collection<File> all) {
+        File[] children = file.listFiles();
+        if (children != null) {
+            for (File child : children) {
+
+                String path = child.getPath();
+
+                if (!path.startsWith("./.")) {
+                    all.add(child);
+                    addTree(child, all);
+                }
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     * @param request request
+     * @param response response
+     * @throws ServletException request
+     * @throws IOException exception
+     */
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
         doGet(request, response);
     }
 
